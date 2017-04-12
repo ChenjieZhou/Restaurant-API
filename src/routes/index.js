91fa52ae3efc39858f4631f7db4364cd0772df75
Chenjie Zhou <czhou33@ucsc.edu>
2017-04-12 00:21:38 -0700

    [feat] Add post request and router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,18 @@ import express from 'express';
 import config from '../config';
 import middleware from '../middleware';
 import initializeDb from '../db';
+import restaurant from '../controller/restaurant'
 
-let route = express();
+let router = express();
 
 //connect to database
 initializeDb(db => {
 
   //internal middleware
-  route.use(middleware({config, db}));
+  router.use(middleware({config, db}));
   //api routers v1 (/v1)
+
+  router.use('/restaurant', restaurant({config, db}));
 })
 
 export default router;
